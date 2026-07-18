c6ede96eec55e2140ad601bd450b95a6e6fbebfa
dharankumar <dharankumar>
2026-07-18 10:49:24 +0530

    updated

---
+++ REACT/Day12/Formhandling/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+
+  const [username,setUsername]=useState("")
+
+  return (
+  <input type="text" placeholder='Enter the number' />  
+  )
+}
+
+export default App
