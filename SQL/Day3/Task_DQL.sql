bc5f743c207380fc1801134b869d9d28c2b63ef7
dharankumar <dharankumar>
2026-07-30 11:55:56 +0530

    updated

---
+++ SQL/Day3/Task_DQL.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE Dql_Task;
---
+++ SQL/Day4/Aggricate_fn.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE employeDetails;
+USE employeDetails;
+
+CREATE TABLE Employinfo (
+EmployeId INT PRIMARY KEY,
+EmployeName VARCHAR(20),
+EmployDepartment VARCHAR);
